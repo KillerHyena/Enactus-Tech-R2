7c1679ecc9226bf3f4c41f04a4a789c31115e843
KillerHyena <crunchvelvet72@gmail.com>
2025-10-16 02:52:06 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 // App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
 import { EventProvider } from './src/context/EventContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { testFirebaseConnection } from './src/utils/testFirebase';
 
 export default function App() {
+  useEffect(() => {
+    // Test Firebase connection on app start
+    testFirebaseConnection();
+  }, []);
+
   return (
     <PaperProvider>
       <AuthProvider>
